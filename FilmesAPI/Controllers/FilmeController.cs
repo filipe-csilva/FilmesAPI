0047fea41a489e1204de5b9c05b1224afff92c3d
filipeleasing <filipepaulocs@gmail.com>
2022-10-12 22:57:56 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+
+    }
+}
