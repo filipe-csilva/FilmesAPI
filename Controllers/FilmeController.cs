ba875dc0ab5aafdd0f8fd2ce5f8e4135ce25ecf8
filipeleasing <filipepaulocs@gmail.com>
2022-10-17 22:50:53 -0300

    1.0.3

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,98 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ReadFilmeDto = FilmesAPI.Data.Dtos.ReadFilmeDto;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        //private static List<Filme> filmes = new List<Filme>();
+        //private static int id = 1;
+
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public FilmeController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
+        {
+            Filme filme = _mapper.Map<Filme>(filmeDto);
+            //filme.Id = id++;
+            //filmes.Add(filme);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new {Id = filme.Id}, filme);
+
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return /*filmes*/_context.Filmes;
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+            //foreach(Filme filme in filmes){
+            //    if(filme.Id == id)
+            //    {
+            //        return filme;
+            //    }
+            //}
+            //return null;
+
+            Filme filme = /*filmes*/_context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if(filme != null)
+            {
+                ReadFilmeDto filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(filmeDto, filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            _context.Filmes.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FilmesAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+    }
+}
